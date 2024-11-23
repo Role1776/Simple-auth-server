969b8495ef30769b95cc6a2f177ea89acd551caa
Role1776 <197003zzz197003zzz@gmail.com>
2024-11-23 23:23:26 +0300

    jwt.go

--- utils/jwt.go
+++ utils/jwt.go
@@ -9,18 +9,18 @@ import (
 
 var MySignKey = []byte("oyminirole")
 
-func GenerateJWT() (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
+
+func GenerateJWT() (string,error) {
+	token := jwt.New(jwt.SigningMethodHS384)
 
 	claims := token.Claims.(jwt.MapClaims)
 	claims["exp"] = time.Now().Add(time.Hour * 800).Unix()
 	claims["user"] = "oyminirole"
 
-	tokenString, err := token.SignedString(MySignKey)
-
+	stringToken,err := token.SignedString(MySignKey)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-
-	return tokenString, nil
+	
+	return stringToken, nil
 }
